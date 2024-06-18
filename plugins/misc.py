c3d59654c374969dbc818b49ee591b5991dd19fc
KangerOnly <117170541+Ling-ex@users.noreply.github.com>
2024-06-18 15:07:01 +0700

    improve layout to misc

--- plugins/misc.py
+++ plugins/misc.py
@@ -6,14 +6,14 @@ from pyrogram import Client, filters, types
 
 @Client.on_message(filters.command("del", config.prefix) & filters.me)
 async def delete(c: Client, m: types.Message):
-    reply = m.reply_to_message
+    if not (reply := m.reply_to_message):
+        return await m.delete()
     await asyncio.gather(reply.delete(), m.delete())
 
 
-@Client.on_message(filters.command("purge", config.prefix) & filters.me)
+@Client.on_message(filters.command("purge", config.prefix) & filters.me & ~filters.private)
 async def purge(c: Client, m: types.Message):
-    reply = m.reply_to_message
-    if not reply:
+    if not (reply := m.reply_to_message):
         return await m.reply("Reply to the message you want to delete next")
     await m.delete()
     chat = m.chat.id
