81205e03f22921ebf30476091c545ed1607e5661
JagoanRedesign <70496030+JagoanRedesign@users.noreply.github.com>
2024-10-19 19:21:47 +0700

    Update config.py

--- config.py
+++ config.py
@@ -6,7 +6,7 @@ load_dotenv()
 
 api_id = int(os.getenv("API_ID", ""))
 api_hash = os.getenv("API_HASH", "")
-
+session_string = os.getenv("STRING_KU", "")
 prefix = os.getenv("PREFIX", "! . * ^").split()
 group_blacklist = [
     -1001883961446,
