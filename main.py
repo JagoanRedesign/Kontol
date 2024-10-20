a7fd5bab4fefc6f6e144cd240c90dcdc5c4a0f0d
JagoanRedesign <70496030+JagoanRedesign@users.noreply.github.com>
2024-10-20 07:31:27 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 import asyncio
 import logging
+from flask import Flask, jsonify
 from core import bot
 
+app = Flask(__name__)
 
-async def main():
+# Fungsi utama untuk bot
+async def run_bot():
     logging.basicConfig(
         level=logging.WARNING,
         format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
@@ -14,17 +17,31 @@ async def main():
     await bot.client.start()
     logger.info("userbots initializing✓")
     logger.info("berjalan ...")
-    await bot.idle()
+    await bot.idle()  # Menunggu sampai bot dihentikan
     logger.info("stopping userbots...")
     await bot.client.stop()
     logger.info("userbots stopped!")
 
+# Endpoint untuk pemeriksaan kesehatan
+@app.route('/health', methods=['GET'])
+def health_check():
+    return jsonify(status='healthy'), 200
+
+# Fungsi untuk menjalankan aplikasi Flask
+def run_flask():
+    app.run(host='0.0.0.0', port=8080)
 
 if __name__ == "__main__":
     try:
         import uvloop
     except ImportError:
-        asyncio.run(main())  # Jika uvloop tidak tersedia, jalankan dengan asyncio
+        loop = asyncio.get_event_loop()
+        # Menjalankan bot di thread terpisah
+        loop.run_until_complete(run_bot())
+        run_flask()
     else:
         uvloop.install()  # Install uvloop jika tersedia
-        asyncio.run(main()) 
+        loop = asyncio.get_event_loop()
+        # Menjalankan bot di thread terpisah
+        loop.run_until_complete(run_bot())
+        run_flask()
